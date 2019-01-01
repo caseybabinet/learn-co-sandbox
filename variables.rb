6d71b4a6f11d70156e82b4293f589d306f8883fb
Casey Babinet <caseybabinet@gmail.com>
2019-01-01 19:50:22 +0000

    Automatically backed up by Learn

---
+++ variables.rb
@@ -0,0 +1,22 @@
+'This is data, it is a string. Strings start with " "'
+
+"Part of being data, or a string, is that ruby doesn't interpret it."
+
+puts 1+1
+puts "1+1"
+
+example = "The word 'example' is equal to this sentence, it's a named variable."
+
+puts example
+puts example
+puts example
+
+puts "variables are any previously undefined word that"
+puts "starts with a lowercase letter."
+
+first_number = 7
+second_number = 14
+
+sum = first_number + second_number
+
+puts sum
